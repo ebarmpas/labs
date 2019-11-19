eade80a62567fd871966777c2182c0f0b08c472c
ebarmpas <ebarmpas@citycollege.sheffield.eu>
2019-11-19 09:19:06 +0200

    Uploaded NumberMethods.java

---
+++ NumberMethods.java
@@ -0,0 +1,28 @@
+public class NumberMethods
+{
+	public static void main(String[]args){
+
+    }
+
+    public static int  minimumInt(int a, int b){
+
+    }
+    public static int median(int a, int b, int c){
+
+    }
+    public static double average(int a, int b, int c, int d, int e){
+
+    }
+    public static int isEven(int num){
+
+    }
+    public static int sumEvenNumbersUpTo(int upTo){
+
+    }
+    public static double percentage(int a, int b){
+
+    }
+    public static double(int a, int b){
+    	
+    }
+}	

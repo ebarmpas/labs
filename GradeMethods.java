91837970cde3a9f597f4c3a63c83ab0ff8c87d33
ebarmpas <ebarmpas@citycollege.sheffield.eu>
2019-11-19 09:33:26 +0200

    Uploaded GradeMethods.java

---
+++ GradeMethods.java
@@ -0,0 +1,17 @@
+public class GradeMethods
+{
+	public static void main(String[]args){
+
+    }
+    public static int promtpForGrade(int grade){
+
+    }
+    public static boolean isPassGrade(int grade){
+
+    }
+    public static boolean isFirstClass(int grade){
+    	
+    }
+
+
+}

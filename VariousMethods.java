93322015f76cf7a8e1907c025620b3627dbafff1
ebarmpas <ebarmpas@citycollege.sheffield.eu>
2019-11-19 10:08:31 +0200

    Uploaded VariousMethods.java

---
+++ VariousMethods.java
@@ -0,0 +1,20 @@
+public class VariousMethods
+{
+	public static void main(String[]args){
+
+    }
+    public static String(int numberOfMonth){
+
+    }
+    public static boolean(int year){
+
+    }
+    public static double fahrenheitToCelsius(int fahrenheit){
+
+    }
+    public static double celsiusToFahreinheit(int celsius){
+    	
+    }
+
+
+}

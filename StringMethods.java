1455c701cfcad2d2e6bdc95c1598fbb2a9b4e77c
ebarmpas <ebarmpas@citycollege.sheffield.eu>
2019-11-19 09:26:08 +0200

    Uploaded StringMethods.java

---
+++ StringMethods.java
@@ -0,0 +1,16 @@
+public class StringMethods
+{
+	public static void main(String[]args){
+
+    }
+
+    public static boolean(String str){
+
+    }
+    public static int(String str){
+
+    }
+    public static void(String message, int upTo){
+
+    }
+}
